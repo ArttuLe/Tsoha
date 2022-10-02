38c6c85885610a98fc25c468a6986209e31ce207
ArttuLeh <lehtonen.arttu@yahoo.fi>
2022-10-02 14:19:52 +0300

    Register/login logic works without db

--- src/app.py
+++ src/app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import getenv
+import os
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-app.secret_key = getenv("SECRET_KEY")
+app.secret_key = 'asdsa3214'#os.environ.get("SECRET_KEY")
 
 db = SQLAlchemy(app)
 
--- src/route.py
+++ src/route.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import Flask
-from flask import render_template, request
+from flask import render_template, request, redirect, flash, url_for
 from os import getenv
 
 @app.route("/")
@@ -8,9 +8,67 @@ def index():
     return render_template("index.html")
 
 @app.route("/main")
-def main():
+def home():
     return render_template("main.html")
+
+@app.route("/register")
+def register_page():
+    return render_template("register.html")
+
+@app.route("/register_user", methods=["POST"])
+def register():
+
+    username = request.form["username"]
+    password = request.form["pword"]
+
+    flash("User registered successfully")
+
+    return redirect(url_for('index'))
 
 @app.route("/login")
 def login_page():
     return render_template("login.html")
+
+@app.route("/login_user", methods=["POST"])
+def login():
+    logged = False
+
+    if request.method == 'POST':
+        
+    username = request.form["username"]
+    password = request.form["pword"]
+
+    if logged:
+        return render_template("main.html")
+    else:
+        flash("Username or password incorrect!")
+        return redirect(request.referrer)
+
+@app.route("/add")
+def add_new():
+    return render_template("add_new.html")
+
+@app.route("/add_expense", methods=["POST"])
+def add_expense():
+    
+    name = request.form["name"]
+    category = request.form["category"]
+    date = request.form["date"]
+    amount = request.form["amount"]
+    info = request.form["info"]
+
+    #DB execute
+
+    flash("Submission added successfully!")
+
+    return redirect(request.referrer)
+
+
+
+@app.route("/view")
+def view():
+    return render_template("view.html")
+
+@app.route("/view_expenses")
+def view_expenses():
+    pass
