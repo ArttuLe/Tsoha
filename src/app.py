1e22e5b28e43af86ee73ad59e1be7e8aa951addb
ArttuLeh <lehtonen.arttu@yahoo.fi>
2022-10-09 18:45:56 +0300

    heroku fix v2

--- src/app.py
+++ src/app.py
@@ -1,13 +1,5 @@
 from flask import Flask
-from os import getenv
 
 app = Flask(__name__)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-
-app.secret_key = getenv("SECRET_KEY")
 
 import route
--- src/db.py
+++ src/db.py
@@ -2,5 +2,15 @@ from app import app
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.secret_key = getenv("SECRET_KEY")
+db_uri = getenv("DATABASE_URL")
+
+if db_uri.startswith("postgres://"):
+    db_uri = db_uri.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+
 db = SQLAlchemy(app)
